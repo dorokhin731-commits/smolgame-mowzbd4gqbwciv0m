1668990e476a73eba3c3d1bbc24358b9b52e1a5d
dorokhin731-commits <dorokhin731@gmail.com>
2026-05-08 18:57:03 +0500

    Add js/config.js

---
+++ js/config.js
@@ -0,0 +1,9 @@
+const gameConfig = {
+    gameWidth: 320,
+    gameHeight: 480,
+    blocksize: 20,
+    framerate: 60,
+    backgroundColor: 0x111111,
+    fontColor: 0xffffff,
+    fontFamily: 'Arial',
+};
